1d8a2bda6d5b2a2d1156b88b6c029a106f982541
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-12 14:35:08 -0700

    post test passing

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const People = require('../lib/models/People');
 
-describe('testing something', () => {
-  it('creates a person qith /people', ()=> {
-    request(app)
+describe('testing app', () => {
+  afterAll(() => {
+    return People.drop();
+  });
+  it('creates a person with /people', ()=> {
+    return request(app)
       .post('/people')
       .send({ name: 'carl', age: 22, color: 'purple'})
       .then(res => {
