55addab1bc8e392688c133e049dc46873cdf8383
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-12 11:58:13 -0700

    file cleanup/pseudo coding

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,16 @@ const app = require('../lib/app');
 
 describe('testing something', () => {
   it('creates a person qith /people', ()=> {
-    request
-      .post('/people');
+    request(app)
+      .post('/people')
+      .send({ name: 'carl', age: 22, color: 'purple'})
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'carl',
+          age: 22,
+          color: 'purple',
+          _id: expect.any(String)
+        });
+      });
   });
 });
